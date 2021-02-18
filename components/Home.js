89a20e8e56b10f15191e376a208e259dd56a9429
masatodejima0413 <masatodejima@gmail.com>
2021-02-18 23:04:16 +0900

    set up sass and create bare home page

---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.background}>
+      <h1 className={styles.title}>masatodejima.com</h1>
+    </div>
+  );
+};
+
+export default Home;
