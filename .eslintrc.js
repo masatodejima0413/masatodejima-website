1766b39ba66eb67498c6c0b49c14c71d9816181c
masatodejima0413 <masatodejima@gmail.com>
2021-02-23 16:36:29 +0900

    typescript install

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,8 +12,17 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  root: true,
   rules: {
-    'eslint-disable react/prop-types': false,
+    '@typescript-eslint/no-non-null-assertion': 2,
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/explicit-function-return-type': 2,
+    '@typescript-eslint/typedef': 2,
   },
 }
